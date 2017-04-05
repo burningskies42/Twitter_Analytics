00908f8ffa4823c34f3eedcb20ef10256eddbe77
burningskies42 <Leon@DESKTOP-UFCJS6E.xapio.de>
2017-04-05 21:26:06 +0200

    EOD 05.04.2017

--- Python/twitter_api/test.py
+++ Python/twitter_api/test.py
@@ -1,7 +1,11 @@
-import quandl, math
+import quandl, math, datetime
 import pandas as pd,numpy as np
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 def quandl_key():
     api_key = pd.read_pickle('quandl_key.pickle')
@@ -18,14 +22,15 @@ df.fillna(-99999, inplace=True)
 
 forecast_out = int(math.ceil(0.01 * len(df)))
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
-
 
 X = np.array(df.drop(['label'],1))
-y = np.array(df['label'])
-
 X = preprocessing.scale(X)
 
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=0.2)
@@ -34,4 +39,24 @@ clf = LinearRegression(n_jobs=-1)
 # clf = svm.SVR(kernel='poly')
 clf.fit(X_train,y_train)
 accuracy = clf.score(X_test,y_test)
-print(accuracy)
+
+forecast_set = clf.predict(X_lately)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()

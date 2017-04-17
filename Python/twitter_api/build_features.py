b169488127933c7ddfec4d46679fa678da599657
Leon <l.edelmann@xapio.de>
2017-04-17 11:08:32 +0200

    deleted tutorial folder

--- Python/twitter_api/build_features.py
+++ Python/twitter_api/build_features.py
@@ -2,7 +2,14 @@ from tweets_to_df import tweet_json_to_df
 from emoticons_parser import emoticons_score
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+# from nltk import pos_tag
+import nltk
 import pandas as pd
+import time
+
+from textblob import Blobber
+from textblob_aptagger import PerceptronTagger
+tb = Blobber(pos_tagger=PerceptronTagger())
 
 # the class will produce and contain all features
 # not in use
@@ -35,24 +42,112 @@ class tweet_fetures():
 def tokenize_and_filter(sentence):
     sentence = word_tokenize(sentence)
     stop_words = set(stopwords.words('english'))
-    words = [w for w in sentence if w.lower() not in stop_words]
-    return words
+    words = []
+    for w in sentence:
+        if w.lower() not in stop_words:
+                words.append(w)
+    # words = [w for w in sentence if w.lower() not in stop_words]
+    tagged = pos_tag(words)
+    return tagged
+
+def has_pronoun(text):
+    # tagged_words = pos_tag(word_list)
+    # start = time.time()
+    tagged_words = tb(text)
+    # dur = time.time() - start
+    # print('tokenize:', dur)
+
+    # start = time.time()
+    # dc = dict(tagged_words.tags)
+    # dur = time.time() - start
+    # print('convert to dict:', dur)
+
+    # start = time.time()
+
+    dc = [x[1] for x in tagged_words.pos_tags]
+
+    # dur = time.time() - start
+    # print('build pos list:', dur)
+
+    # start = time.time()
+    ret = ('PRP' in dc)
+    # dur = time.time() - start
+    # print('scanning dict:', dur)
+
+    return ret
+
+def count_upper(text):
+    return sum(1 for c in text if c.isupper())/len(text)
 
 def build_feature_df(df):
-    df['words'] = df['text'].apply(lambda x : tokenize_and_filter(x))
+
+    start = time.time()
+    df['words'] = df['text'].apply(lambda x : word_tokenize(x))
+    dur = time.time() - start
+    print('tokenize words:',dur)
+
+    start = time.time()
+    stop_words = set(stopwords.words('english'))
+    df['words'] = df['words'].apply(lambda x : [w for w in x if w.lower() not in stop_words])
+    dur = time.time() - start
+    print('filter out stop words:',dur)
+
+    start = time.time()
     df['len_characters'] = df['text'].apply(lambda x : len(x))
+    dur = time.time() - start
+    print('len_characters:', dur)
+
+    start = time.time()
     df['num_words'] = df['words'].apply(lambda x : len(x))
+    dur = time.time() - start
+    print('num_words:', dur)
+
+    start = time.time()
     df['has_question_mark'] = df['text'].apply(lambda x : x.find('?') != -1)
+    dur = time.time() - start
+    print('has_question_mark:', dur)
+
+    start = time.time()
     df['has_exclamation_mark'] = df['text'].apply(lambda x : x.find('!') != -1)
+    dur = time.time() - start
+    print('has_exclamation_mark:', dur)
+
+    start = time.time()
     df['has_multi_quest_exclam'] = df['text'].apply(lambda x : (x.count('?') > 1 or x.count('!') > 1))
+    dur = time.time() - start
+    print('has_multi_quest_exclam:', dur)
+
+    start = time.time()
     df['emotji_sent_score'] = df['text'].apply(lambda x : emoticons_score(x))
-    df = df[['words','len_characters','num_words','has_question_mark',
-        'has_exclamation_mark','has_multi_quest_exclam','emotji_sent_score']]
+    dur = time.time() - start
+    print('emotji_sent_score:', dur)
+
+    start = time.time()
+    df['has_pronoun'] = df['text'].apply(lambda x : has_pronoun(x))
+    dur = time.time() - start
+    print('has_pronoun:', dur)
+
+    start = time.time()
+    df['count_upper'] = df['text'].apply(lambda x : count_upper(x))
+    dur = time.time() - start
+    print('count_upper:', dur)
+
+    df = df[['words',
+        'len_characters','num_words','has_question_mark',
+        'has_exclamation_mark','has_multi_quest_exclam','emotji_sent_score',
+        'has_pronoun','count_upper']]
 
     return df
 
+start = time.time()
 df = tweet_json_to_df('amazon_dataset.json')
+dur = time.time() - start
+print('tweet_json_to_df:', dur)
 
+start = time.time()
 df = build_feature_df(df)
+dur = time.time() - start
+print('\nbuild_feature_df:', dur)
 
-print(df['words'].head())
+pd.DataFrame.to_csv(df,path_or_buf='feature_set.csv',sep=';')
+# print(df.head())
